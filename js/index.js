5ff93f7c4fdc8c7cf4677f04f424e18e1d98cafc
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-24 15:11:39 +0200

    Implementing testimonials section

--- js/index.js
+++ js/index.js
@@ -12,3 +12,21 @@ megaMenu.addEventListener("mouseenter", () => {
 megaMenu.addEventListener("mouseleave", () => {
   megaMenu.classList.remove("show__mega-menu");
 });
+
+// Stars Rating
+
+const personsRating = [...document.querySelectorAll(".person__rating")];
+const stars = (person) => {
+  let html = ``;
+  const goldStars = +person.dataset.star;
+  for (let i = 0; i < goldStars; i++) {
+    html += `<i class="fa-solid rated fa-star"></i>`;
+  }
+  for (let i = 0; i < 5 - goldStars; i++) {
+    html += `<i class="fa-regular  fa-star"></i>`;
+  }
+
+  person.innerHTML = html;
+};
+
+personsRating.forEach(stars);
